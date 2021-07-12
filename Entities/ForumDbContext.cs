e3851f541a91b4b233f03d90b06a53cd1ffb23ba
Gloomy-stack <karl.yotl@gmail.com>
2021-07-12 17:37:46 +0200

    Added basic entities

---
+++ Entities/ForumDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Entities
+{
+    public class ForumDbContext : DbContext
+    {
+        private string _connectionString = 
+            "Server=(localdb)\\mssqllocaldb;Database=ForumDb;Trusted_Connection=True;";
+    }
+}
